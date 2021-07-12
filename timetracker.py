6c17ed1b1c072da1b8d8b872fc0c06e29078cbd8
Mark Steve Samson <marksteve@thinkingmachin.es>
2021-07-12 10:54:54 +0800

    Speed up by switching to the Reports API

--- timetracker.py
+++ timetracker.py
@@ -7,33 +7,33 @@ from requests.auth import HTTPBasicAuth
 from toolz import groupby, valmap
 
 
-def get_entries(token, start, end):
+def get_entries(token, workspace_id, client_id, start, end):
     start_date = start.set(hour=0, minute=0, second=0, microsecond=0)
-    end_date = end.add(days=1).set(hour=0, minute=0, second=0, microsecond=0)
-    entries = requests.get(
-        "https://api.track.toggl.com/api/v8/time_entries",
-        params={"start_date": start_date.isoformat(), "end_date": end_date.isoformat()},
-        auth=HTTPBasicAuth(token, "api_token"),
-    ).json()
-    return entries
+    end_date = end.set(hour=0, minute=0, second=0, microsecond=0)
+    page = 1
+    while True:
+        resp = requests.get(
+            "https://api.track.toggl.com/reports/api/v2/details",
+            params={
+                "user_agent": "Thinking Machines",
+                "workspace_id": workspace_id,
+                "client_ids": client_id,
+                "since": start_date.isoformat(),
+                "until": end_date.isoformat(),
+                "page": page,
+            },
+            auth=HTTPBasicAuth(token, "api_token"),
+        ).json()
+        for entry in resp.get("data", []):
+            yield entry
+        page += 1
+        per_page = resp["per_page"]
+        total_count = resp["total_count"]
+        if page * per_page >= total_count:
+            break
 
 
-def get_projects(token, entries):
-    project_ids = {e.get("pid") for e in entries if e.get("pid") is not None}
-    projects_info = {pid: get_project_by_id(pid, token=token) for pid in project_ids}
-    projects = {k: v["name"] for k, v in projects_info.items()}
-    return projects
-
-
-def get_project_by_id(id, token):
-    project = requests.get(
-        f"https://api.track.toggl.com/api/v8/projects/{id}",
-        auth=HTTPBasicAuth(token, "api_token"),
-    )
-    return project.json().get("data")
-
-
-def summarize(entries, projects, timezone):
+def summarize(entries, timezone):
     mod_entries = [
         {
             "date": pendulum.parse(e["start"])
@@ -44,16 +44,16 @@ def summarize(entries, projects, timezone):
         for e in entries
     ]
     summary = valmap(
-        lambda e: sum(map(lambda i: i["duration"], e)),
+        lambda e: sum(map(lambda i: i["dur"], e)),
         groupby(
-            key=lambda x: (x["date"], x.get("pid"), x.get("description")),
+            key=lambda x: (x["date"], x.get("project"), x.get("description")),
             seq=mod_entries,
         ),
     )
     formated_summaries = [
         {
             "date": k[0],
-            "project": projects.get(k[1]),
+            "project": k[1],
             "description": k[2],
             "duration": v,
         }
@@ -67,7 +67,7 @@ def format_report(date, summary):
     for entry in summary:
         project = entry["project"] if entry["project"] else "no-project"
         description = entry["description"]
-        duration_hrs = entry["duration"] / 3600
+        duration_hrs = entry["duration"] / 60 / 60 / 1000
         if duration_hrs < 0:
             print(
                 f"WARN: Got negative time for {description}. There might be a running timer"
@@ -95,12 +95,27 @@ def submit_checkins(token, channel, checkins):
 @click.option("--since", type=str)
 @click.option("--until", type=str)
 @click.option("--toggl-token", type=str)
+@click.option("--toggl-workspace-id", type=str)
+@click.option("--toggl-client-id", type=str)
 @click.option("--slack-token", type=str)
 @click.option("--slack-channel", type=str, default="#dailycheckin")
 @click.option("--timezone", type=str, default="Asia/Manila")
-def main(since, until, toggl_token, slack_token, slack_channel, timezone):
+def main(
+    since,
+    until,
+    toggl_token,
+    toggl_workspace_id,
+    toggl_client_id,
+    slack_token,
+    slack_channel,
+    timezone,
+):
     if toggl_token is None or toggl_token == "":
         raise ValueError("Toggl token is needed")
+    if toggl_workspace_id is None or toggl_workspace_id == "":
+        raise ValueError("Toggl workspace id is needed")
+    if toggl_client_id is None or toggl_client_id == "":
+        raise ValueError("Toggl client id is needed")
     now = pendulum.now(timezone)
     if since:
         start = pendulum.parse(since).set(tz=timezone)
@@ -110,9 +125,8 @@ def main(since, until, toggl_token, slack_token, slack_channel, timezone):
         end = pendulum.parse(until).set(tz=timezone)
     else:
         end = now
-    entries = get_entries(toggl_token, start, end)
-    projects = get_projects(toggl_token, entries)
-    summaries = summarize(entries, projects, timezone)
+    entries = get_entries(toggl_token, toggl_workspace_id, toggl_client_id, start, end)
+    summaries = summarize(entries, timezone)
     checkins = []
     for date, summary in summaries.items():
         report = format_report(date, summary)
